010cf297bf797bb8ba62bb32a9c317e8396c6a36
avishj <avish.j@protonmail.com>
2020-12-04 16:22:07 +0530

    Routes Completed + Other Work

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+	<b-navbar toggleable="lg" type="dark" variant="info">
+		<b-navbar-brand href="/">
+			<b-img></b-img>
+		</b-navbar-brand>
+
+		<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+		<b-collapse id="nav-collapse" is-nav>
+			<b-navbar-nav class="ml-auto">
+				<b-nav-item href="https://mfcvit.in/">
+					Our Website
+				</b-nav-item>
+				<b-nav-item to="/contact">Contact Us</b-nav-item>
+				<b-nav-item to="/rules">Rules</b-nav-item>
+				<b-nav-item> </b-nav-item>
+				<b-nav-item> </b-nav-item>
+				<b-nav-item> </b-nav-item>
+			</b-navbar-nav>
+		</b-collapse>
+	</b-navbar>
+</template>
