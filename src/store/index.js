130a428aae4ded4e455198c3995ac30ae55d2096
avishj <avish.j@protonmail.com>
2020-12-15 23:10:25 +0530

    Fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,196 +1,215 @@
-
 // import { resolve,reject } from "core-js/fn/promise";
-import Vue from "vue";
-import Vuex from "vuex";
-import {API} from './axios'
-import axios from 'axios'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { API } from './axios';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 var getAPI = axios.create({
-  baseURL: 'https://mfcrecruitment.herokuapp.com/',
-  timeout: 3000,
-  headers: {
-      Authorization: "Bearer " + localStorage.getItem('accessToken')
-  }
-})
+	baseURL: 'https://mfcrecruitment.herokuapp.com/',
+	timeout: 3000,
+	headers: {
+		Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+	}
+});
 
 export default new Vuex.Store({
-  state: {
-    accessToken: null,
-    refreshToken: null,
-    APIData: '',
-    authenticated: false,
-    Tech: null,
-    Manage: null,
-    Design: null,
-    Edi: null,
-  },
-  mutations: {
-    setAuthentication(state){
-      state.authenticated= !state.authenticated;
-    }
-  },
-  actions: {  
+	state: {
+		accessToken: null,
+		refreshToken: null,
+		APIData: '',
+		authenticated: false,
+		Tech: null,
+		Manage: null,
+		Design: null,
+		Edi: null
+	},
+	mutations: {
+		setAuthentication(state) {
+			state.authenticated = !state.authenticated;
+		}
+	},
+	actions: {
+		getRefreshToken() {
+			return new Promise((resolve, reject) => {
+				API.post('/api/api/token/refresh/', {
+					refresh: localStorage.getItem('refreshToken')
+				})
+					.then(response => {
+						localStorage.setItem(
+							'accessToken',
+							response.data.access
+						);
+						resolve(true);
+					})
+					.catch(error => {
+						reject(error);
+					});
+			});
+		},
 
-    getRefreshToken(){
-      return new Promise((resolve,reject) => {
-        API.post('/api/api/token/refresh/', {  
-          refresh: localStorage.getItem('refreshToken')
-        })
-        .then(response => {
-          localStorage.setItem('accessToken', response.data.access)
-          resolve(true);
-        })
-        .catch(error => {
-          reject(error) 
-      })
-    })
-    },
+		userLogin(context, usercredentials) {
+			// eslint-disable-next-line
+			return new Promise((resolve, reject) => {
+				API.post('/api/login/', {
+					email: usercredentials.email,
+					password: usercredentials.password
+				})
+					.then(response => {
+						context.commit('setAuthentication');
+						localStorage.setItem(
+							'accessToken',
+							response.data.tokens.access
+						);
+						localStorage.setItem(
+							'refreshToken',
+							response.data.tokens.refresh
+						);
+						resolve(true);
+					})
+					.catch(error => {
+						reject(error);
+					});
+			});
+		},
 
-    userLogin(context, usercredentials) {
-       // eslint-disable-next-line
-      return new Promise((resolve,reject) => {
-        API.post('/api/login/', {  
-          email: usercredentials.email,
-          password: usercredentials.password 
-        })
-        .then(response => {
-          context.commit('setAuthentication')
-          localStorage.setItem('accessToken', response.data.tokens.access)
-          localStorage.setItem('refreshToken', response.data.tokens.refresh)
-          resolve(true);
-        })
-        .catch(error => {
-             reject(error)  
-        })
-      })
-    },
+		userRegister(context, usercredentials) {
+			return new Promise((resolve, reject) => {
+				API.post('/api/register/', {
+					email: usercredentials.email,
+					username: usercredentials.username,
+					phone_number: usercredentials.phone,
+					password: usercredentials.password,
+					reg_no: usercredentials.reg_no
+				})
+					// eslint-disable-next-line
+					.then(({ data, status }) => {
+						if (status === 201) {
+							resolve(true);
+						}
+					})
+					.catch(error => {
+						if (error.response.status === 400) {
+							reject(
+								new Error(
+									'User with this email already exists.'
+								),
+								null
+							);
+						} else reject(error.response.data.message);
+					});
+			});
+		},
 
-    userRegister(context, usercredentials) {
-      return new Promise((resolve, reject) => {
-        API.post('/api/register/', {
-          email: usercredentials.email,
-          username: usercredentials.username,
-          phone_number: usercredentials.phone, 
-          password: usercredentials.password,
-          reg_no: usercredentials.reg_no
-         })
-        // eslint-disable-next-line
-        .then(({ data, status }) => {
-          if (status === 201) {
-            resolve(true);
-          }
-        })
-        .catch(error => {
-          if (error.response.status === 400)
-          {
-            reject(new Error("User with this email already exists."), null);
-          }
-          else reject(error.response.data.message);
-        });
-      })
-    },
- 
-    userVerify(context, usercredentials) {
-      // eslint-disable-next-line
-      return new Promise((resolve,reject) => {
-        API.post('/api/email-verify/', {
-          email: usercredentials.email,
-          otp: parseInt(usercredentials.otp)
-        })
-        // eslint-disable-next-line
-        .then(({ data, status }) => {
-          if (status === 201) {
-            resolve(true);
-          }
-        })
-        .catch(error => {
-          if (error.response.status === 400)
-          {
-            reject(new Error("Invalid Email or OTP. Try Again !!"), null);
-          }
-          else reject(error.response.data.message);
-        });
-      })
-    },
+		userVerify(context, usercredentials) {
+			// eslint-disable-next-line
+			return new Promise((resolve, reject) => {
+				API.post('/api/email-verify/', {
+					email: usercredentials.email,
+					otp: parseInt(usercredentials.otp)
+				})
+					// eslint-disable-next-line
+					.then(({ data, status }) => {
+						if (status === 200) {
+							resolve(true);
+						}
+					})
+					.catch(error => {
+						if (error.response.status === 400) {
+							reject(
+								new Error('Invalid Email or OTP. Try Again !!'),
+								null
+							);
+						} else reject(error.response.data.message);
+					});
+			});
+		},
 
-    async userLogout(context, {dispatch}) {
-      await dispatch('getRefreshToken')
-        getAPI = axios.create({
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem('accessToken')
-            }
-          })
-        return new Promise((resolve, reject) => {
-          getAPI.post('https://mfcrecruitment.herokuapp.com/api/logout/', {
-              refresh: localStorage.getItem('refreshToken')
-            })
-            // eslint-disable-next-line
-            .then(response => {
-              context.commit('setAuthentication')
-              resolve(true);
-            })
-            .catch(error => {
-                reject(error)
-            })
-        })
-    },
+		async userLogout(context, { dispatch }) {
+			await dispatch('getRefreshToken');
+			getAPI = axios.create({
+				headers: {
+					Authorization:
+						'Bearer ' + localStorage.getItem('accessToken')
+				}
+			});
+			return new Promise((resolve, reject) => {
+				getAPI
+					.post('https://mfcrecruitment.herokuapp.com/api/logout/', {
+						refresh: localStorage.getItem('refreshToken')
+					})
+					// eslint-disable-next-line
+					.then(response => {
+						context.commit('setAuthentication');
+						resolve(true);
+					})
+					.catch(error => {
+						reject(error);
+					});
+			});
+		},
 
-    async getTest({dispatch}){
-        await dispatch('getRefreshToken')
-        getAPI = axios.create({
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem('accessToken')
-            }
-          })
-        getAPI.get('https://mfcrecruitment.herokuapp.com/api/user_test/')
-          .then(response => 
-            {
-              localStorage.setItem('Tech', response.data.technical),
-              localStorage.setItem('Manage', response.data.management),
-              localStorage.setItem('Edi', response.data.editorial),
-              localStorage.setItem('Design', response.data.design)
-            })
-    },
+		async getTest({ dispatch }) {
+			await dispatch('getRefreshToken');
+			getAPI = axios.create({
+				headers: {
+					Authorization:
+						'Bearer ' + localStorage.getItem('accessToken')
+				}
+			});
+			getAPI
+				.get('https://mfcrecruitment.herokuapp.com/api/user_test/')
+				.then(response => {
+					localStorage.setItem('Tech', response.data.technical),
+						localStorage.setItem(
+							'Manage',
+							response.data.management
+						),
+						localStorage.setItem('Edi', response.data.editorial),
+						localStorage.setItem('Design', response.data.design);
+				});
+		},
 
-    async getTechnical({dispatch}){
-      await dispatch('getRefreshToken')
-      getAPI = axios.create({
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem('accessToken')
-            }
-          })
-    },
+		async getTechnical({ dispatch }) {
+			await dispatch('getRefreshToken');
+			getAPI = axios.create({
+				headers: {
+					Authorization:
+						'Bearer ' + localStorage.getItem('accessToken')
+				}
+			});
+		},
 
-    async getManagement({dispatch}){
-      await dispatch('getRefreshToken')
-      getAPI = axios.create({
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem('accessToken')
-            }
-          })
-    },
+		async getManagement({ dispatch }) {
+			await dispatch('getRefreshToken');
+			getAPI = axios.create({
+				headers: {
+					Authorization:
+						'Bearer ' + localStorage.getItem('accessToken')
+				}
+			});
+		},
 
-    async getDesign({dispatch}){
-      await dispatch('getRefreshToken')
-      getAPI = axios.create({
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem('accessToken')
-            }
-          })
-    },
+		async getDesign({ dispatch }) {
+			await dispatch('getRefreshToken');
+			getAPI = axios.create({
+				headers: {
+					Authorization:
+						'Bearer ' + localStorage.getItem('accessToken')
+				}
+			});
+		},
 
-    async getEdi({dispatch}){
-      await dispatch('getRefreshToken')
-      getAPI = axios.create({
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem('accessToken')
-            }
-          })
-    },
-  },
-  modules: {
-
-  }
+		async getEdi({ dispatch }) {
+			await dispatch('getRefreshToken');
+			getAPI = axios.create({
+				headers: {
+					Authorization:
+						'Bearer ' + localStorage.getItem('accessToken')
+				}
+			});
+		}
+	},
+	modules: {}
 });
