6a48d84bfbd20f904a3c93b35402dba0d717a71c
Avish Jha <avish.j@protonmail.com>
2021-07-24 14:30:19 +0530

    Fix Backend

--- src/views/Backend.vue
+++ src/views/Backend.vue
@@ -8,105 +8,27 @@
         <br />
         <br />
         <br />
-        <!-- <div v-for="item in this.questions.mcq" :key="item.question_id">
-          <b-img-lazy class="site-backend-img my-2" fluid-grow :src="item.question"></b-img-lazy>
-          <div class="site-backend-mcqopt pl-5 text-moz-orange my-2">
-            <input
-              type="radio"
-              :ref="item.question_id"
-              v-model="item.question_id"
-              :value="item.option_1"
-              checked
-            />
-            <label class="p-2 h5">{{item.option_1}}</label>
-            <br />
-            <input
-              type="radio"
-              :ref="item.question_id"
-              v-model="item.question_id"
-              :value="item.option_2"
-            />
-            <label class="p-2 h5">{{item.option_2}}</label>
-            <br />
-            <input
-              type="radio"
-              :ref="item.question_id"
-              v-model="item.question_id"
-              :value="item.option_3"
-            />
-            <label class="p-2 h5">{{item.option_3}}</label>
-            <br />
-            <input
-              type="radio"
-              :ref="item.question_id"
-              v-model="item.question_id"
-              :value="item.option_4"
-            />
-            <label class="p-2 h5">{{item.option_4}}</label>
-          </div>
-          <br />
-          <br />
-        </div>-->
-        <div v-for="item in this.questions.mcq" :key="item.question_id">
+        <div v-for="item in this.questions" :key="item.question_id">
           <b-img-lazy
-            class="site-backend-img my-2"
-            fluid-grow
-            :src="item.question"
-          ></b-img-lazy>
-          <b-form-group
-            class="site-backend-mcqopt pl-4 text-moz-orange my-2"
-            v-slot="{ ariaDescribedby }"
-          >
-            <b-form-radio-group
-              v-model="item.question_id"
-              :aria-describedby="ariaDescribedby"
-              :name="item.question_id"
-            >
-              <b-form-radio :value="item.option_1">{{
-                item.option_1
-              }}</b-form-radio>
-              <br />
-              <b-form-radio :value="item.option_2">{{
-                item.option_2
-              }}</b-form-radio>
-              <br />
-              <b-form-radio :value="item.option_3">{{
-                item.option_3
-              }}</b-form-radio>
-              <br />
-              <b-form-radio :value="item.option_4">{{
-                item.option_4
-              }}</b-form-radio>
-              <b-form-radio class="d-none" value="NA"
-                >Not Answered</b-form-radio
-              >
-            </b-form-radio-group>
-          </b-form-group>
-          <br />
-          <br />
-        </div>
-        <div v-for="item in this.questions.write" :key="item.question_id">
-          <b-img-lazy
-            class="site-backend-img my-2"
+            class="site-backend-img m-2"
             fluid-grow
             :src="item.question"
           ></b-img-lazy>
           <textarea
             rows="5"
-            class="site-backend-ta my-2 px-5 py-4"
+            class="site-backend-ta m-2 px-5 py-4"
             :id="item.question_id"
             placeholder="Type your answer here!"
           >
-Type your answer here!</textarea
-          >
+          Type your answer here!
+          </textarea>
           <br />
           <br />
         </div>
-        <br />
         <b-button
           :disabled="isSubmitted"
-          class="my-2"
           @click="sendAnswers"
+          class="m-2"
           block
           pill
           variant="moz-orange"
@@ -125,28 +47,6 @@ export default {
     return {
       questions: [],
       answers: [],
-      tech_mcq_1: "NA",
-      tech_mcq_2: "NA",
-      tech_mcq_3: "NA",
-      tech_mcq_4: "NA",
-      tech_mcq_5: "NA",
-      tech_mcq_6: "NA",
-      tech_mcq_7: "NA",
-      tech_mcq_8: "NA",
-      tech_mcq_9: "NA",
-      tech_mcq_10: "NA",
-      tech_mcq_11: "NA",
-      tech_mcq_12: "NA",
-      tech_mcq_13: "NA",
-      tech_mcq_14: "NA",
-      tech_mcq_15: "NA",
-      tech_mcq_16: "NA",
-      tech_mcq_17: "NA",
-      tech_mcq_18: "NA",
-      tech_mcq_19: "NA",
-      tech_mcq_20: "NA",
-      tech_mcq_21: "NA",
-      tech_mcq_22: "NA",
       isSubmitted: false
     };
   },
@@ -159,47 +59,37 @@ export default {
     async sendAnswers() {
       this.isSubmitted = true;
       // eslint-disable-next-line
-			var getAPI = axios.create({
+      var getAPI = axios.create({
         baseURL: "https://mfcrecruitment.herokuapp.com/",
         timeout: 3000,
         headers: {
           Authorization: "Bearer " + localStorage.getItem("accessToken")
         }
       });
-      this.questions.mcq.forEach(item => {
+      this.questions.forEach(item => {
         this.answers.push({
-          domain: "1",
-          question: item.question,
-          answer: item.question_id
-        });
-      });
-      this.questions.write.forEach(item => {
-        this.answers.push({
-          domain: "1",
+          domain: "2",
           question: item.question,
           answer: document.getElementById(item.question_id).value
         });
       });
+      this.$store.dispatch("getBackend").catch(err => {
+        console.log(err);
+      });
       // eslint-disable-next-line
-			return new Promise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
         getAPI
           .post(
-            "https://mfcrecruitment.herokuapp.com/api/send_backend_responses/",
+            "https://mfcrecruitment.herokuapp.com/api/send_back_responses/",
             this.answers
           )
           // eslint-disable-next-line
-					.then((response) => {
+          .then(response => {
             this.$router.push({ name: "TestThanks" });
           })
           .catch(error => {
             alert(error);
           });
-      });
-    },
-    getValueOfRadio(rad) {
-      var ele = document.getElementsByName(rad);
-      ele.forEach(element => {
-        if (element.checked) return element.value;
       });
     }
   },
@@ -218,7 +108,7 @@ export default {
       getAPI
         .get("https://mfcrecruitment.herokuapp.com/api/backendquestions/")
         .then(response => {
-          this.questions = response.data;
+          this.questions = response.data.write;
           resolve(true);
         })
         .catch(error => {
@@ -233,12 +123,9 @@ export default {
 .site-backend-img {
   opacity: 0.8;
 }
-.site-backend-mcqopt {
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 10px;
-}
 .site-backend-ta {
   border-radius: 10px;
   width: 100%;
+  font-size: 1.25rem;
 }
 </style>
