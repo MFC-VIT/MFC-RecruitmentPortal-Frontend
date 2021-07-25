ddc864f90742bb36b8e880f8540e333d94a5df4d
Avish Jha <avish.j@protonmail.com>
2021-07-25 20:05:29 +0530

    Updated Test Page

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -6,72 +6,96 @@
       </h3>
     </center>
     <b-row class="text-white no-gutters" align-h="center">
-      <b-col md="5">
+      <b-col md="6">
         <div class="site-test-card m-2" @click="takeFrontend">
           <div class="d-flex justify-content-center align-content-center">
             <h1 class="text-center p-2">
               Frontend
             </h1>
           </div>
-          <h5 class="text-justify px-3 pb-4">
+          <h5 class="text-center px-3 pb-4">
             Its primarily focused on conversion of data to graphical interface
             thereby enabling easy User Interaction with the website.
           </h5>
         </div>
       </b-col>
-      <b-col md="5">
+      <b-col md="6">
         <div class="site-test-card m-2" @click="takeBackend">
           <div class="d-flex justify-content-center align-content-center">
             <h1 class="text-center p-2">
               Backend
             </h1>
           </div>
-          <h5 class="text-justify px-3 pb-4">
-            Its primarily focused on how the site works and how the database
-            information is communicated to the browser.
+          <h5 class="text-center px-3 pb-4">
+            Its primarily focused on how the site works, some filtering and how
+            the database information is communicated to the browser.
           </h5>
         </div>
       </b-col>
     </b-row>
-    <b-row class="text-white" align-h="center">
-      <b-col>
+    <b-row class="text-white no-gutters" align-h="center">
+      <b-col md="4">
+        <div class="site-test-card m-2" @click="takeUIUX">
+          <div class="d-flex justify-content-center align-content-center">
+            <h1 class="text-center p-2">
+              UI/UX
+            </h1>
+          </div>
+          <h5 class="text-center px-3 pb-4">
+            UI/UX is used to design the flow of user interface and determines
+            the experience of the user.
+          </h5>
+        </div>
+      </b-col>
+      <b-col md="4">
         <div class="site-test-card m-2" @click="takeDesign">
           <div class="d-flex justify-content-center align-content-center">
             <h1 class="text-center p-2">
-              Design & Media
+              Graphic Design
             </h1>
           </div>
-          <h5 class="text-justify px-3 pb-4">
-            Design is all about creating, fashioning, executing, or constructing
-            according to a plan. Software tools like Photoshop and Figma are
-            used. Media involves working with video editing tools, VFX,
+          <h5 class="text-center px-3 pb-4">
+            Graphic Design is all about creating, fashioning, executing, or
+            constructing according to a plan.
+          </h5>
+        </div>
+      </b-col>
+      <b-col md="4">
+        <div class="site-test-card m-2" @click="takeVideo">
+          <div class="d-flex justify-content-center align-content-center">
+            <h1 class="text-center p-2">
+              Video Editing
+            </h1>
+          </div>
+          <h5 class="text-center px-3 pb-4">
+            Video Editing involves working with video editing tools, VFX,
             animations and a lot more cool stuff!
           </h5>
         </div>
       </b-col>
     </b-row>
     <b-row class="text-white no-gutters" align-h="center">
-      <b-col md="5">
+      <b-col md="6">
         <div class="site-test-card m-2" @click="takeApp">
           <div class="d-flex justify-content-center align-content-center">
             <h1 class="text-center p-2">
               App
             </h1>
           </div>
-          <h5 class="text-justify px-3 pb-4">
+          <h5 class="text-center px-3 pb-4">
             Its prime objective is to create software applications for operating
             on mobile devices such as tablets, smartphones and smart watches.
           </h5>
         </div>
       </b-col>
-      <b-col md="5">
+      <b-col md="6">
         <div class="site-test-card m-2" @click="takeML">
           <div class="d-flex justify-content-center align-content-center">
             <h1 class="text-center p-2">
               ML
             </h1>
           </div>
-          <h5 class="text-justify px-3 pb-4">
+          <h5 class="text-center px-3 pb-4">
             It's based on data analysis that automates analytical model
             building, identifying patterns and making decisions with minimal
             human intervention.
@@ -181,8 +205,14 @@ export default {
     takeBackend() {
       this.$router.push({ name: "BackendRules" });
     },
+    takeUIUX() {
+      this.$router.push({ name: "UIUXRules" });
+    },
     takeDesign() {
       this.$router.push({ name: "DesignRules" });
+    },
+    takeVideo() {
+      this.$router.push({ name: "VideoRules" });
     },
     takeApp() {
       this.$router.push({ name: "AppRules" });
