232ef9ff0432de4f6ec37f6b3d82832e139fc5ad
Avish Jha <avish.j@protonmail.com>
2021-07-25 18:15:43 +0530

    Added Links to Test

--- src/views/Frontend.vue
+++ src/views/Frontend.vue
@@ -9,11 +9,13 @@
         <br />
         <br />
         <div v-for="item in this.questions" :key="item.question_id">
-          <b-img-lazy
-            class="site-frontend-img m-2"
-            fluid-grow
-            :src="item.question"
-          ></b-img-lazy>
+          <a :href="item.link" target="_blank">
+            <b-img-lazy
+              class="site-frontend-img m-2"
+              fluid-grow
+              :src="item.question"
+              :href="item.link"
+            ></b-img-lazy>
           <textarea
             rows="5"
             class="site-frontend-ta m-2 px-5 py-4"
