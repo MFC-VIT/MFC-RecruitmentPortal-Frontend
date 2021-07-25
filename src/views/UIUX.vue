1ed85bdb0ba26c45c4dfcd52ac8771a30eed8303
Avish Jha <avish.j@protonmail.com>
2021-07-25 19:56:22 +0530

    Updated domain IDs

--- src/views/UIUX.vue
+++ src/views/UIUX.vue
@@ -71,7 +71,7 @@ export default {
       });
       this.questions.forEach(item => {
         this.answers.push({
-          domain: "10",
+          domain: "6",
           question: item.question,
           answer: document.getElementById(item.question_id).value
         });
