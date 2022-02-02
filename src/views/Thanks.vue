07c8b2cac4216f04858abe0081b571b8a24d20a0
Avish Jha <avish.j@protonmail.com>
2022-02-03 00:20:04 +0530

    FIXXXXXXXXXXXXXXXXX

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -2,7 +2,8 @@
   <b-container>
     <center>
       <h2 class="mt-5 text-moz-orange">
-        Thanks for registering! See you on 26th!
+        Thanks for registering! Ready for your tests? The portal is live till
+        6th February, 2022 at 11:59PM!
       </h2>
       <br />
       <b-button to="/user/login" pill variant="moz-orange">
